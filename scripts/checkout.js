bfb0ee5c8e023836ca0b9e88de758f2128bc690e
Rahul <rahulsharma2rk2329@gmail.com>
2023-11-03 19:48:09 +0530

    make checkout delete button responsive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrecncy } from "./utils/money.js";
 
@@ -13,7 +13,9 @@ cart.forEach((cartItem) => {
       matchingProduct = product;
     }
   });
-  cartSummaryHTML += `<div class="cart-item-container">
+  cartSummaryHTML += `<div class="cart-item-container js-cart-item-container-${
+    matchingProduct.id
+  }">
     <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
     <div class="cart-item-details-grid">
@@ -36,7 +38,9 @@ cart.forEach((cartItem) => {
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
+            matchingProduct.id
+          }">
             Delete
           </span>
         </div>
@@ -86,3 +90,14 @@ cart.forEach((cartItem) => {
 });
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
 console.log(cartSummaryHTML);
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+
+    const conatiner = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    conatiner.remove();
+  });
+});
